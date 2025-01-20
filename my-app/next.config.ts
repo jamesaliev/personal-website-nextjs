ad99c0b72cd4607022e17c42cb18d9acea4043c7
James Aliev <james.aliev@gmail.com>
2025-01-19 20:45:21 -0500

    changes based on som tutorial

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  assetPrefix: '/personal-website-nextjs',
+  basePath: '/personal-website-nextjs',
 };
 
 export default nextConfig;
